55accaf09f9ca4ef2607ae408c3cb06377781394
Amal pk <amalashok7902@gmail.com>
2023-02-25 19:47:39 +0530

    splash

--- lib/controller/splash_controller/splash_controller.dart
+++ lib/controller/splash_controller/splash_controller.dart
@@ -1,35 +1,51 @@
-import 'dart:developer';
+// import 'dart:developer';
 
+// import 'package:authentication/view/sign_in/sign_in.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+// class SplashController with ChangeNotifier {
+//   FlutterSecureStorage storage = const FlutterSecureStorage();
+//   String? onboardValue;
+//   String? signInValue;
+
+//   void splash(context) async {
+//     await Future.delayed(
+//       const Duration(seconds: 3),
+//     );
+
+//     onboardValue = await storage.read(key: 'onboard');
+//     signInValue = await storage.read(key: 'token');
+//     log(signInValue.toString());
+//     if (signInValue != null) {
+//       log('message');
+//       Navigator.of(context).pushAndRemoveUntil(
+//           MaterialPageRoute(
+//             builder: (context) => SignIn(),
+//           ),
+//           (route) => false);
+//     } else if (onboardValue != null) {
+//       Navigator.of(context).pushAndRemoveUntil(
+//           MaterialPageRoute(
+//             builder: (context) => SignIn(),
+//           ),
+//           (route) => false);
+//     }
+//   }
+// }
 import 'package:authentication/view/sign_in/sign_in.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SplashController with ChangeNotifier {
-  FlutterSecureStorage storage = const FlutterSecureStorage();
-  String? onboardValue;
-  String? signInValue;
-
-  void splash(context) async {
+  void splash(BuildContext context) async {
     await Future.delayed(
       const Duration(seconds: 3),
     );
 
-    onboardValue = await storage.read(key: 'onboard');
-    signInValue = await storage.read(key: 'token');
-    log(signInValue.toString());
-    if (signInValue != null) {
-      log('message');
-      Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(
-            builder: (context) => SignIn(),
-          ),
-          (route) => false);
-    } else if (onboardValue != null) {
-      Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(
-            builder: (context) => SignIn(),
-          ),
-          (route) => false);
-    }
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => SignIn(),
+      ),
+    );
   }
 }
